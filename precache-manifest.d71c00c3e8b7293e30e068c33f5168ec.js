ba967900cca20029c158f1a2378d2089e3738576
Manoj <manoj.gudivada1@gmail.com>
2020-05-31 20:58:18 +0530

    Updates

---
+++ precache-manifest.d71c00c3e8b7293e30e068c33f5168ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0f2027e437a86799d4379795a803c5",
+    "url": "/YoutubeClone/index.html"
+  },
+  {
+    "revision": "85c80418e13d7575189e",
+    "url": "/YoutubeClone/static/css/main.7bedad0a.chunk.css"
+  },
+  {
+    "revision": "2aabdc3e89753d72bbcf",
+    "url": "/YoutubeClone/static/js/2.0c32bf67.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/YoutubeClone/static/js/2.0c32bf67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c80418e13d7575189e",
+    "url": "/YoutubeClone/static/js/main.7b5e19fa.chunk.js"
+  },
+  {
+    "revision": "bf48991f97dd4cecc983",
+    "url": "/YoutubeClone/static/js/runtime-main.d50a79fe.js"
+  }
+]);
